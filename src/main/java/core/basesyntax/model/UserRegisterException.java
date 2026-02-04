2dffbf77ca66d9f557ac934ee37e980f146bd6e4
alex <ursusandwolf@protonmail.com>
2026-02-04 15:51:10 +0200

    UserRegisterException

---
+++ src/main/java/core/basesyntax/model/UserRegisterException.java
@@ -0,0 +1,8 @@
+package core.basesyntax.model;
+
+public class UserRegisterException extends RuntimeException {
+
+    public UserRegisterException(String s) {
+        super(s);
+    }
+}
--- src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
+++ src/main/java/core/basesyntax/service/RegistrationServiceImpl.java
@@ -3,12 +3,42 @@ package core.basesyntax.service;
 import core.basesyntax.dao.StorageDao;
 import core.basesyntax.dao.StorageDaoImpl;
 import core.basesyntax.model.User;
+import core.basesyntax.model.UserRegisterException;
 
 public class RegistrationServiceImpl implements RegistrationService {
+    public static final int MIN_LENGTH = 6;
     private final StorageDao storageDao = new StorageDaoImpl();
 
     @Override
     public User register(User user) {
-        return null;
+        String login = user.getLogin();
+        validateLength(login, "Login");
+        checkIfUserRegistered(login);
+
+        validateLength(user.getPassword(), "Password");
+        checkAge(user.getAge());
+
+
+        return user;
+    }
+
+    private void checkIfUserRegistered(String login) {
+        User storageUser = storageDao.get(login);
+        if (storageUser != null) {
+            throw new UserRegisterException
+                    ("User with such login already registered");
+        }
+    }
+
+    private void checkAge(Integer age) {
+        if (age < 18) {
+            throw new UserRegisterException("Age is less than 18");
+        }
+    }
+
+    private static void validateLength(String s, String name) {
+        if (s == null || s.length() < MIN_LENGTH) {
+            throw new UserRegisterException(name + " length less " + MIN_LENGTH + " characters");
+        }
     }
 }
